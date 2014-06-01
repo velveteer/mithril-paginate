7172d738b1202d6456574ca03317dffca954f9ba
Josh <notjoshmiller@gmail.com>
2014-05-31 22:30:12 -0500

    Refactor into CommonJS module and expose ctrl/view as children to parent modules.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,4 @@
-paginate = new Mpaginate();
-paginate2 = new Mpaginate();
+var mpaginate = require('./mpaginate.js');
 
 // Mock some data
 var list1 = [];
@@ -7,140 +6,7 @@ for (var i = 0; i<50; i++) {
     list1.push({'Random': i*Math.random(), 'Name':'John #'+i, 'Clone': i, 'Born': new Date(2014, 2, i)})
 }
 
-var randomJSON = [
-    {
-        "id": 0,
-        "guid": "94d891a2-5bc5-448c-8ece-63a1c9744231",
-        "isActive": true,
-        "balance": "$3,897.86",
-        "picture": "http://placehold.it/32x32",
-        "age": 24,
-        "eyeColor": "brown",
-        "name": "Lola Blevins",
-        "gender": "female",
-        "company": "MOREGANIC",
-        "email": "lolablevins@moreganic.com",
-        "phone": "+1 (815) 462-2910",
-        "registered": "2014-01-25T10:34:20 +06:00",
-        "latitude": -36.185287,
-        "longitude": 179.57708,
-        "tags": [
-            "fugiat",
-            "laborum",
-            "elit",
-            "deserunt",
-            "excepteur",
-            "est",
-            "consequat"
-        ]
-    },
-    {
-        "id": 1,
-        "guid": "7af5a0b6-dbac-4a8a-ac6a-a328560ff69e",
-        "isActive": false,
-        "balance": "$1,608.81",
-        "picture": "http://placehold.it/32x32",
-        "age": 20,
-        "eyeColor": "blue",
-        "name": "West Dunn",
-        "gender": "male",
-        "company": "XEREX",
-        "email": "westdunn@xerex.com",
-        "phone": "+1 (911) 535-2791",
-        "registered": "2014-02-16T17:15:55 +06:00",
-        "latitude": -1.72706,
-        "longitude": 155.638468,
-        "tags": [
-            "reprehenderit",
-            "dolore",
-            "nulla",
-            "eiusmod",
-            "laboris",
-            "cupidatat",
-            "aliquip"
-        ]
-    },
-    {
-        "id": 2,
-        "guid": "67cb9ece-518c-4c5c-9312-945a5c1ca93c",
-        "isActive": false,
-        "balance": "$3,936.09",
-        "picture": "http://placehold.it/32x32",
-        "age": 27,
-        "eyeColor": "brown",
-        "name": "Edna Hoover",
-        "gender": "female",
-        "company": "EXTRAGENE",
-        "email": "ednahoover@extragene.com",
-        "phone": "+1 (811) 567-3514",
-        "registered": "2014-05-02T22:44:48 +05:00",
-        "latitude": -30.638981,
-        "longitude": 145.236228,
-        "tags": [
-            "fugiat",
-            "irure",
-            "excepteur",
-            "veniam",
-            "aliqua",
-            "cupidatat",
-            "tempor"
-        ]
-    },
-    {
-        "id": 3,
-        "guid": "d471cf6b-cd13-415c-a6eb-fea957611c7f",
-        "isActive": true,
-        "balance": "$3,719.32",
-        "picture": "http://placehold.it/32x32",
-        "age": 34,
-        "eyeColor": "green",
-        "name": "Brenda Hickman",
-        "gender": "female",
-        "company": "GOLISTIC",
-        "email": "brendahickman@golistic.com",
-        "phone": "+1 (940) 509-2302",
-        "registered": "2014-04-30T04:51:24 +05:00",
-        "latitude": -44.32226,
-        "longitude": -158.394294,
-        "tags": [
-            "exercitation",
-            "sint",
-            "et",
-            "nisi",
-            "deserunt",
-            "minim",
-            "nostrud"
-        ]
-    },
-    {
-        "id": 4,
-        "guid": "586d509a-1664-4cf3-a848-8a5cc9e5e5d2",
-        "isActive": false,
-        "balance": "$3,131.31",
-        "picture": "http://placehold.it/32x32",
-        "age": 21,
-        "eyeColor": "blue",
-        "name": "Alison Sutton",
-        "gender": "female",
-        "company": "INTRADISK",
-        "email": "alisonsutton@intradisk.com",
-        "phone": "+1 (827) 530-3665",
-        "registered": "2014-02-17T12:50:08 +06:00",
-        "latitude": -14.750654,
-        "longitude": -59.497017,
-        "tags": [
-            "quis",
-            "sit",
-            "deserunt",
-            "ut",
-            "ipsum",
-            "voluptate",
-            "dolor"
-        ]
-    }
-]
-
-// Get list async
+// Get list async -- m.request can also be used here
 var getList = function(list) {
     m.startComputation();
     var deferred = m.deferred();
@@ -151,21 +17,35 @@ var getList = function(list) {
     return deferred.promise;
 };
 
-// Set custom headers
-paginate.headers(['Name', 'Clone', 'Born', 'Even', 'Random']);
+// Module namespace
+var grid = {};
 
-// Set values to display
+// Model
+grid.list = m.prop([]);
+
+// Cell functions
 var nameLink = function() { return [ m('a', {href: 'profile/' + this.Name}, this.Name) ]};
 var isEven = function() { return this.Clone % 2 ? 'no' : 'yes'};
-paginate.cells([nameLink, 'Clone', 'Born', isEven, 'Random']);
 
-// Set rows per page
-paginate.rowsPerPage(5);
-paginate2.rowsPerPage(1);
+// Set mpaginate options
+grid.options = {
+    headers: ['Name', 'Clone', 'Born', 'Even', 'Random'],
+    cells: [nameLink, 'Clone', 'Born', isEven, 'Random']
+}
 
-// Get list async then paginate the list
-getList(list1).then(paginate.list);
-getList(randomJSON).then(paginate2.list);
+// Inject mpaginate controller into parent controller
+grid.controller = function() {
+    this.mpaginate = new mpaginate.controller(grid.list, grid.options);
+}
 
-m.module(document.getElementById('table'), paginate);
-m.module(document.getElementById('table2'), paginate2);
+// Hook mpaginate view into parent view
+grid.view = function(ctrl) {
+    return mpaginate.view(ctrl.mpaginate);
+}
+
+// Get list async then populate model
+getList(list1).then(grid.list)
+
+// Instantiate Mithril model
+m.module(document.getElementById('table'), grid);
+
--- src/scripts/mpaginate.js
+++ src/scripts/mpaginate.js
@@ -1,113 +1,102 @@
-// Constructor function for module
-function Mpaginate() {
+var m = require('mithril');
 
-    // Namespace for module
-    var mpaginate = {};
+var mpaginate = {};
 
-    function range(start, stop) {
-        var length = Math.max(Math.ceil((stop - start)), 0);
-        var idx = 0;
-        var range = new Array(length);
-        while(idx < length) { range[idx++] = start; ++start; }
-        return range;
-    }
-
-    function keys(list) {
-        return function() {
-            return typeof(list()[0]) === 'object' ? Object.keys(list()[0]) : []
-        }
-    }
-
-    // Public variables (model)
-    mpaginate.rowsPerPage = m.prop(5); // How many rows to show per page
-    mpaginate.list = m.prop([]); // A list of objects to be paginated
-    mpaginate.headers = m.prop([]); // List of headers for <th> elements
-    mpaginate.cells = m.prop([]); // List of values to show for <td> elements
-    mpaginate.rowAttr = m.prop({}); // An object for styling each <tr>
-
-    // Controller
-    mpaginate.controller = function() {
-        this.currentPage = m.prop(1); // Current page is reset on each load
-        this.startPage = m.prop(0); // The first page the controls start on
-        this.endPage = m.prop(5); // The last page to show in the controls
-        this.rowsPerPage = mpaginate.rowsPerPage;
-        this.list = mpaginate.list;
-        this.rowAttr = mpaginate.rowAttr;
-
-        // Use defined headers or pull keys from the object schema
-        this.headers = mpaginate.headers().length > 0 ? mpaginate.headers : keys(this.list);
-        // Use defined values for each cell or pull values from object schema
-        this.cells = mpaginate.cells().length > 0 ? mpaginate.cells : keys(this.list);
-
-        // Slice list into pages
-        this.paginated = function() {
-            return this.list().slice((this.currentPage()-1) * this.rowsPerPage(),
-                ((this.currentPage()-1)*this.rowsPerPage()) + this.rowsPerPage())
-        }.bind(this);
-
-        // Calculate how many pages to generate
-        this.numPages = function() {
-            var numPages = 0;
-            if (this.list() !== null && this.rowsPerPage() !== null) {
-                numPages = Math.ceil(this.list().length / this.rowsPerPage());
-            }
-            return range(1, numPages+1);
-        }.bind(this);
-
-        // Show next pages in controls
-        this.nextPage = function() {
-            var start = this.startPage();
-            var end = this.endPage();
-            if (this.endPage() !== this.numPages()) {
-                this.startPage(start + 1);
-                this.endPage(end + 1);
-            }
-        }.bind(this);
-
-        // Show previous pages in controls
-        this.prevPage = function() {
-            var start = this.startPage();
-            var end = this.endPage();
-            if (this.startPage() > 0) {
-                this.startPage(start - 1);
-                this.endPage(end - 1);
-            }
-        }.bind(this);
-    }
-
-    // View
-    mpaginate.view = function(ctrl) {
-        return [
-            m('table.table', [
-                m('thead', [
-                    m('tr', [
-                        ctrl.headers().map(function(header, index) {
-                            return m('th', header)
-                        })
-                    ])
-                ]),
-                m('tbody', [
-                    ctrl.paginated().map(function(item, index) {
-                        return m('tr', typeof ctrl.rowAttr() === 'function' ? ctrl.rowAttr().call(item) : ctrl.rowAttr(),
-                            [ ctrl.cells().map(function(cell, index) {
-                                return typeof cell === 'function' ?  m('td', cell.call(item)) : m('td', item[cell]) })
-                        ])
-                    })
-                ]),
-            ]),
-            m('ul.list-inline.text-center', [
-                ctrl.startPage() > 0 ? m('li', [ m('a.btn.btn-default.btn-sm', {onclick: ctrl.prevPage}, '<<' )]) : null,
-                ctrl.numPages().length < 2 ? null : ctrl.numPages().slice(ctrl.startPage(), ctrl.endPage()).map(function(page, index) {
-                    return m('li', [
-                        m('a', {
-                            onclick: m.withAttr('innerHTML', ctrl.currentPage),
-                            class: ctrl.currentPage() == page ? 'btn btn-default btn-sm active' : 'btn btn-default btn-sm'}, page)
-                        ])
-                    }),
-                ctrl.numPages().length > ctrl.endPage() ? m('li', [ m('a.btn.btn-default.btn-sm', {onclick: ctrl.nextPage}, '>>')]) : null
-            ])
-        ]
-    }
-
-    return mpaginate;
+function range(start, stop) {
+    var length = Math.max(Math.ceil((stop - start)), 0);
+    var idx = 0;
+    var range = new Array(length);
+    while(idx < length) { range[idx++] = start; ++start; }
+    return range;
 }
+
+function keys(list) {
+    return function() {
+        return typeof list()[0] === 'object' ? Object.keys(list()[0]) : []
+    }
+}
+
+// Controller
+mpaginate.controller = function(list, options) {
+    var options = options || {};
+
+    this.rowsPerPage = options.rowsPerPage && options.rowsPerPage.length > 0 ? m.prop(options.rowsPerPage) : m.prop(5); // Number of rows per page
+    this.headers = options.headers && options.headers.length > 0 ? m.prop(options.headers) : keys(list); // List of headers for <th> elements
+    this.cells = options.cells && options.cells.length > 0 ? m.prop(options.cells) : keys(list); // List of values to show for <td> elements
+    this.rowAttr = options.rowAttr ? m.prop(options.rowAttr) : m.prop({}); // Object to apply attributes to <tr> elements
+
+    this.currentPage = m.prop(1); // Current page is reset on each load
+    this.startPage = m.prop(0); // The first page the controls start on
+    this.endPage = m.prop(5); // The last page to show in the controls
+
+    // Slice list into pages
+    this.paginated = function() {
+        return list().slice((this.currentPage()-1) * this.rowsPerPage(),
+            ((this.currentPage()-1)*this.rowsPerPage()) + this.rowsPerPage());
+    }.bind(this);
+
+    // Calculate how many pages to generate
+    this.numPages = function() {
+        var numPages = 0;
+        if (list() !== null && this.rowsPerPage() !== null) {
+            numPages = Math.ceil(list().length / this.rowsPerPage());
+        }
+        return range(1, numPages+1);
+    }.bind(this);
+
+    // Show next pages in controls
+    this.nextPage = function() {
+        var start = this.startPage();
+        var end = this.endPage();
+        if (this.endPage() !== this.numPages()) {
+            this.startPage(start + 1);
+            this.endPage(end + 1);
+        }
+    }.bind(this);
+
+    // Show previous pages in controls
+    this.prevPage = function() {
+        var start = this.startPage();
+        var end = this.endPage();
+        if (this.startPage() > 0) {
+            this.startPage(start - 1);
+            this.endPage(end - 1);
+        }
+    }.bind(this);
+}
+
+// View
+mpaginate.view = function(ctrl) {
+    return [
+        m('table.table', [
+            m('thead', [
+                m('tr', [
+                    ctrl.headers().map(function(header) {
+                        return m('th', header)
+                    })
+                ])
+            ]),
+            m('tbody', [
+                ctrl.paginated().map(function(item) {
+                    return m('tr', typeof ctrl.rowAttr() === 'function' ? ctrl.rowAttr().call(item) : ctrl.rowAttr(),
+                        [ ctrl.cells().map(function(cell) {
+                            return typeof cell === 'function' ?  m('td', cell.call(item)) : m('td', item[cell]) })
+                    ])
+                })
+            ]),
+        ]),
+        m('ul.list-inline.text-center', [
+            ctrl.startPage() > 0 ? m('li', [ m('a.btn.btn-default.btn-sm', {onclick: ctrl.prevPage}, '<<' )]) : null,
+            ctrl.numPages().length < 2 ? null : ctrl.numPages().slice(ctrl.startPage(), ctrl.endPage()).map(function(page, index) {
+                return m('li', [
+                    m('a', {
+                        onclick: m.withAttr('innerHTML', ctrl.currentPage),
+                        class: ctrl.currentPage() == page ? 'btn btn-default btn-sm active' : 'btn btn-default btn-sm'}, page)
+                    ])
+                }),
+            ctrl.numPages().length > ctrl.endPage() ? m('li', [ m('a.btn.btn-default.btn-sm', {onclick: ctrl.nextPage}, '>>')]) : null
+        ])
+    ]
+}
+
+module.exports = mpaginate;
